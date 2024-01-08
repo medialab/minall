5a6609112cb48094afbf06ede07cf5197b2bc09c
Kelly Christensen <kelly.christensen@sciencespo.fr>
2024-01-08 17:10:41 +0100

    docstring for ontological subtype SocialMediaPost

--- minall/enrichment/other_social_media/__init__.py
+++ minall/enrichment/other_social_media/__init__.py
@@ -1,1 +1,6 @@
+# minall/enrichment/other_social_media/__init__.py
+
+"""Module to update ontological subtype for social media posts whose data is not accessible.
+"""
+
 from minall.enrichment.other_social_media.add_data import add_data as add_type_data
--- minall/enrichment/other_social_media/add_data.py
+++ minall/enrichment/other_social_media/add_data.py
@@ -1,3 +1,11 @@
+# minall/enrichment/other_social_media/add_data.py
+
+"""Module contains function to write web content ontological subtype information to CSV.
+
+The module contains a function that write the ontological subtype "SocialMediaPosting" and the related target URL to a CSV, which will be inserted into the 'links' SQL table.
+"""
+
+
 import csv
 from pathlib import Path
 from typing import List
@@ -6,6 +14,12 @@ from minall.tables.links import LinksConstants
 
 
 def add_data(data: List[str], outfile: Path):
+    """For the set of target URLs, write the URL and the category "SocialMediaPosting" to a CSV row for insert into the 'links' SQL table.
+
+    Args:
+        data (List[str]): Target URLs.
+        outfile (Path): Path to CSV file for links.
+    """
     with open(outfile, "w") as f:
         writer = csv.DictWriter(f, fieldnames=LinksConstants.col_names)
         writer.writeheader()
