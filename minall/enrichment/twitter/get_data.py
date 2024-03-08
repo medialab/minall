c87b2aa89414885640e9460ecdd675506649314c
Yomguithereal <guillaumeplique@gmail.com>
2024-03-08 14:34:58 +0100

    Reverting to single-threaded tweet hydration

--- minall/enrichment/twitter/get_data.py
+++ minall/enrichment/twitter/get_data.py
@@ -3,8 +3,6 @@
 """Module contains functions for collecting, normalizing, and writing data from Twitter.
 """
 
-from concurrent.futures import ThreadPoolExecutor
-from itertools import islice
 from pathlib import Path
 from typing import List
 
@@ -29,32 +27,18 @@ def get_twitter_data(
         links_writer, shared_content_writer, progress = contexts
 
         t = progress.add_task(description="[bold blue]Querying Tweets", total=len(data))
+        scraper = TweetScraper()
 
-        for batch in batched(data, 10):
-            scraper = TweetScraper()
-            with ThreadPoolExecutor() as executor:
-                for url, tweet in executor.map(scraper, batch):
-                    formatted_tweet = NormalizedTweet.from_payload(url=url, tweet=tweet)
-                    links_writer.writerow(formatted_tweet.as_csv_dict_row())
+        for url in data:
+            url, tweet = scraper(url)
+            formatted_tweet = NormalizedTweet.from_payload(url=url, tweet=tweet)
+            links_writer.writerow(formatted_tweet.as_csv_dict_row())
 
-                    # Write the shared content data
-                    for shared_link in parse_shared_content(url=url, tweet=tweet):
-                        if shared_link:
-                            shared_content_writer.writerow(
-                                shared_link.as_csv_dict_row()
-                            )
+            # Write the shared content data
+            for shared_link in parse_shared_content(url=url, tweet=tweet):
+                if shared_link:
+                    shared_content_writer.writerow(
+                        shared_link.as_csv_dict_row()
+                    )
 
-                    progress.advance(t)
-
-
-def batched(iterable, n):
-    """Batch data into lists of length n. The last batch may be shorter.
-
-    Function implemeneted in Python 3.12 itertools. Solution taken from Python issue at [https://github.com/python/cpython/issues/98363](https://github.com/python/cpython/issues/98363).
-    """
-    # batched('ABCDEFG', 3) --> ABC DEF G
-    if n < 1:
-        raise ValueError("n must be >= 1")
-    it = iter(iterable)
-    while batch := list(islice(it, n)):
-        yield batch
+            progress.advance(t)
